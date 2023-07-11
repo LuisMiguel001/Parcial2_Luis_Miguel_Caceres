7c1c24e5900e7982773e56aa73898aaf28fa1007
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-10 20:02:36 -0400

    Agregando la clase Productos.

---
+++ Shared/Models/Productos.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parcial2_Luis_Miguel_Caceres.Shared.Models
+{
+	public class Productos
+	{
+		public int ProductoId { get; set; }
+
+		public string Descripcion { get; set; }
+
+        public int Tipo { get; set; }
+
+        public int Existencia { get; set; }
+    }
+}
