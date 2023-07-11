b99e9cabc53bbba1fc149ac514710f9f23c3cb4d
Luis Miguel Jerez Cáceres <luis_jerez1@ucne.edu.do>
2023-07-10 20:11:11 -0400

    Agregando la foreignKey que me faltaba en la clase Productos.

--- Shared/Models/Productos.cs
+++ Shared/Models/Productos.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +16,8 @@ namespace Parcial2_Luis_Miguel_Caceres.Shared.Models
         public int Tipo { get; set; }
 
         public int Existencia { get; set; }
+
+		[ForeignKey("ProductoId")]
+		public ICollection<EntradasDetalle> EntradasDetalles { get; set; } = new List<EntradasDetalle>();
     }
 }
